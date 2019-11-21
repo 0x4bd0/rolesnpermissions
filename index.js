96c00355f569c0d4c43c7f60d56f5137fc38d94d
0x4bd0 <abdoumjr@gmail.com>
2019-11-21 14:32:05 +0100

    update

--- index.js
+++ index.js
@@ -1,2 +1,161 @@
+const _ = require('lodash');
 
-module.export('rnp');
+var perm = function(user) {
+    if (typeof user !== "object") throw new TypeError("Perm requires a valide object parameter");
+    this.user=user;
+    this.logic=true;
+    this.checked=0;
+    this.continue=1;
+  };
+
+  perm.prototype.can = function(permission) {
+
+    if(this.continue==1)
+    {
+        const reducedPermissions=[];
+        _.forEach(this.user.data.permissions, function(value) {
+            reducedPermissions.push(value.name)
+          });
+          if(typeof permission =="string")
+          {
+        if(_.includes(reducedPermissions, permission))
+            {
+                this.logic*=true
+            } 
+            else{
+                this.logic*=false
+            }
+          }
+          else if(typeof permission =="object")
+          {
+            if(_.difference(permission,reducedPermissions).length === 0)
+            {
+                this.logic*=true
+            } 
+            else{
+                this.logic*=false
+            }
+          }
+          this.checked=1;
+          this.logic==false?this.continue=0:""
+
+    }
+    return this;
+    
+  };
+
+  perm.prototype.inTeam = function(team) {
+    if(this.continue==1)
+    {
+        const reducedPermissions=[];
+        _.forEach(this.user.data.services, function(value) {
+            reducedPermissions.push(value._id)
+          });
+          if(typeof team =="string")
+          {
+            if(_.includes(reducedPermissions, team))
+            {
+                this.logic*=true
+            } 
+            else{
+                this.logic*=false
+            }
+          }
+          else if(typeof team =="object")
+          {
+            if(_.difference(team,reducedPermissions).length === 0)
+            {
+                this.logic*=true
+            } 
+            else{
+                this.logic*=false
+            }
+          }
+          this.checked=1;
+          this.logic==false?this.continue=0:""
+    
+    }
+    return this;
+     
+  };
+
+  perm.prototype.inRegion = function(region) {
+    if(this.continue==1)
+    {
+        const reducedPermissions=[];
+        _.forEach(this.user.data.regions, function(value) {
+            reducedPermissions.push(value._id)
+          });
+        if(typeof region =="string")
+        {
+          if(_.includes(reducedPermissions, region))
+          {
+              this.logic*=true
+          } 
+          else{
+              this.logic*=false
+          }
+        }
+        else if(typeof region =="object")
+        {
+          if(_.difference(region,reducedPermissions).length === 0)
+          {
+              this.logic*=true
+          } 
+          else{
+              this.logic*=false
+          }
+        }
+        this.checked=1;
+        this.logic==false?this.continue=0:""
+    
+          
+    }
+   
+    return this;
+};
+
+perm.prototype.inCountry = function(country) {
+    if(this.continue==1)
+    {
+        const reducedPermissions=[];
+        _.forEach(this.user.data.countries, function(value) {
+            reducedPermissions.push(value._id)
+          });
+        if(typeof country =="string")
+        {
+          if(_.includes(reducedPermissions, country))
+          {
+              this.logic*=true
+          } 
+          else{
+              this.logic*=false
+          }
+        }
+        else if(typeof country =="object")
+        {
+          if(_.difference(country,reducedPermissions).length === 0)
+          {
+              this.logic*=true
+          } 
+          else{
+              this.logic*=false
+          }
+        }
+        this.checked=1;
+        this.logic==false?this.continue=0:""
+    
+         
+    }
+ 
+    return this;
+};
+  
+
+  perm.prototype.check = function(res) {  
+    return  this.checked==0?"Please perfom a check first": (this.logic==0?res.status(403).send({
+      message:"Missing privilege!"
+    }):"");
+  };
+
+  module.exports=perm;
